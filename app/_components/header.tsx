b312ffed0d05a370acbf51778cb7dd4f9a2e5a0e
Marlon Ferreira <ferreira.marlon@live.com>
2024-05-21 14:07:06 -0300

    feat: add auth handler to header component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   HomeIcon,
   ListOrderedIcon,
@@ -9,8 +11,20 @@ import {
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const Header = () => {
+  const { data, status } = useSession();
+
+  const handleLoginClick = () => {
+    signIn();
+  };
+
+  const handleLogoutClick = () => {
+    signOut();
+  };
+
   return (
     <div>
       <Card className="flex items-center justify-between p-[1.875rem]">
@@ -24,16 +38,56 @@ const Header = () => {
             <SheetHeader className="text-left text-lg font-semibold">
               Menu
             </SheetHeader>
-            <div className="mt-2 flex flex-col gap-2">
-              <Button variant="outline" className="w-full justify-start gap-3">
-                <LogInIcon size={16} />
-                Fazer Login
-              </Button>
 
-              <Button variant="outline" className="w-full justify-start gap-3">
+            {status === "authenticated" && (
+              <div className="mt-6 flex items-center gap-3">
+                <Avatar>
+                  <AvatarFallback>{data?.user?.name?.[0]}</AvatarFallback>
+                  {data.user?.image && (
+                    <AvatarImage
+                      src={data?.user?.image as string | undefined}
+                      alt={data?.user?.name as string | undefined}
+                    />
+                  )}
+                </Avatar>
+
+                <div>
+                  <h3>{data?.user?.name}</h3>
+                  <span>{data?.user?.email}</span>
+                </div>
+              </div>
+            )}
+
+            {status === "authenticated" ? (
+              <>
+                <Button
+                  onClick={handleLogoutClick}
+                  variant="outline"
+                  className="mt-6 w-full justify-start gap-3"
+                >
+                  <LogInIcon size={16} />
+                  Sair
+                </Button>
+              </>
+            ) : (
+              <div>
+                <h3 className="text-sm mt-6">Olá, seja bem-vindo</h3>
+                <Button
+                  onClick={handleLoginClick}
+                  variant="outline"
+                  className="mt-6 w-full justify-start gap-3"
+                >
+                  <LogInIcon size={16} />
+                  Entrar
+                </Button>
+              </div>
+            )}
+
+            <div className="mt-2 flex flex-col gap-2">
+              {/* <Button variant="outline" className="w-full justify-start gap-3">
                 <HomeIcon size={16} />
                 Inicio
-              </Button>
+              </Button> */}
 
               <Button variant="outline" className="w-full justify-start gap-3">
                 <PercentIcon size={16} />
@@ -59,3 +113,6 @@ const Header = () => {
 };
 
 export default Header;
+function useAuth(): { data: any } {
+  throw new Error("Function not implemented.");
+}
