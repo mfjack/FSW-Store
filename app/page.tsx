db8633dd795bd9b9f8d38aa55191decb4b86db00
Marlon Ferreira <ferreira.marlon@live.com>
2024-05-22 14:05:22 -0300

    feat: add first discount banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 const Home = () => {
-  return <></>;
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-01.svg"
+        alt="até 55% de desconto só esse mês"
+        width={0}
+        height={0}
+        className="h-full w-full"
+      />
+    </div>
+  );
 };
 
 export default Home;
