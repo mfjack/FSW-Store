585dc54272257ffaa5de64e8e47859a250cf3949
Marlon Ferreira <ferreira.marlon@live.com>
2024-05-24 11:49:53 -0300

    feat: add keyboards section to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import Categories from "./_components/categories";
 import ProductList from "./_components/product-list";
 import { db } from "../_lib/prisma";
+import { title } from "process";
+import SectionTitle from "./_components/section-title";
+import PromoBanner from "./_components/promo-banner";
 
 const Home = async () => {
   const deals = await db.product.findMany({
@@ -12,32 +15,33 @@ const Home = async () => {
     },
   });
 
+  const keyboards = await db.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
+
   return (
     <div>
-      <Image
-        src="/banner-01.svg"
-        alt="até 55% de desconto só esse mês"
-        width={0}
-        height={0}
-        className="h-full w-full px-5"
-      />
+      <PromoBanner src="/banner-01.svg" alt="até 55% de desconto só esse mês" />
 
       <div className="mt-6 px-5">
         <Categories />
       </div>
 
       <div className="mt-6">
-        <p className="mb-2 pl-5 font-bold">OFERTAS</p>
+        <SectionTitle>Mouses</SectionTitle>
         <ProductList products={deals} />
       </div>
 
-      <Image
-        src="/banner-03.svg"
-        alt="até 55% de desconto em mouses"
-        width={0}
-        height={0}
-        className="h-full w-full px-5"
-      />
+      <PromoBanner src="/banner-03.svg" alt="até 55% de desconto em mouses" />
+
+      <div className="mt-6">
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboards} />
+      </div>
     </div>
   );
 };
