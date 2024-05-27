22722c720077f83f95f3ccea950828ddb19eca7e
Marlon Ferreira <ferreira.marlon@live.com>
2024-05-27 11:01:47 -0300

    feat: add mouses section to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -23,24 +23,39 @@ const Home = async () => {
     },
   });
 
+  const mouses = await db.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
+
   return (
-    <div>
+    <div className="flex flex-col gap-6">
       <PromoBanner src="/banner-01.svg" alt="até 55% de desconto só esse mês" />
 
-      <div className="mt-6 px-5">
+      <div className="px-5">
         <Categories />
       </div>
 
-      <div className="mt-6">
-        <SectionTitle>Mouses</SectionTitle>
+      <div>
+        <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
+      </div>
+
+      <PromoBanner src="/banner-02.svg" alt="até 55% de desconto em mouses" />
+
+      <div>
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboards} />
       </div>
 
       <PromoBanner src="/banner-03.svg" alt="até 55% de desconto em mouses" />
 
-      <div className="mt-6">
-        <SectionTitle>Teclados</SectionTitle>
-        <ProductList products={keyboards} />
+      <div>
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
       </div>
     </div>
   );
