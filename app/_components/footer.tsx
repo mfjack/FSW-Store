478516124cc6aff9c33c08463b8683063aea75c1
Marlon Ferreira <ferreira.marlon@live.com>
2024-05-27 11:27:55 -0300

    style: home page gap and footer font size

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <p className="bg-accent p-5 text-center text-sm font-semibold opacity-40">
+    <p className="bg-accent p-5 text-center text-xs font-medium opacity-40">
       All rights reserved &copy; {new Date().getFullYear()}
     </p>
   );
